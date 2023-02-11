f2f1783f000298315912c6790021310a5f95f62a
Elmar055 <104317952+Elmar055@users.noreply.github.com>
2023-02-11 22:59:14 +0400

    All files upload

---
+++ DogAndBall/Assets/Challenge 2/Scripts/MoveForwardX.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveForwardX : MonoBehaviour
+{
+    // speed of prefab
+    public float speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // prefab movement
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+}
---
+++ DogAndBall/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    // gameobject variables for create a clone of dog prefab
+    public GameObject dogPrefab;
+    // variable to prevent the dog from running endlessly
+    private float coolDown = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && coolDown <= 0)
+        {
+            // clone and send dogprefab
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            coolDown = 1;
+        }
+        if (coolDown >= 0)
+        {
+            // refresh cooldown
+            coolDown -= Time.deltaTime;
+        }
+        //Debug.Log(Time.deltaTime);
+        //Debug.Log(coolDown);
+    }
+}
